a8900e4eae86b0c19de32586acb7668d09bd6114
wensir365 <wensir365@163.com>
2017-04-11 15:02:17 +0800

    Clean and Merge gaussmod.f90 into legsym.f90

--- src/legsym.f90
+++ src/legsym.f90
@@ -631,4 +631,115 @@ do jlat = 1 , klat / 2
   pa(2*jlat  ) = pr(klat-jlat+1)
 enddo
 pr = pa
-end
+end subroutine altlat
+
+
+
+! =======================================================
+! SUBROUTINE INIGAU
+!
+! cleaned by Xinyu Wen on Apr/11/2017
+! pure functions "ql" and "qld" are incorporated into the
+! main pure function of "inigau", using "contains" block.
+! And, gaussmod.f90 was pasted at the end of legsym.f90
+!
+! pure subroutine "inigau": 计算高斯纬度及权重的初始化
+! =======================================================
+
+pure &
+subroutine inigau(klat,pz0,pzw)                             ! pz0 & pzw are (kind=8) reals !!!
+   implicit none
+   integer,                       intent(in)  :: klat       ! Number of Gaussian latitudes
+   real(kind=8), dimension(klat), intent(out) :: pz0(klat)  ! Gaussian abscissas
+   real(kind=8), dimension(klat), intent(out) :: pzw(klat)  ! Gaussian weights
+
+   ! local
+   integer      , parameter :: NITER = 50                   ! Maximum # of iterations
+   real (kind=8), parameter :: PI    =  3.14159265358979_8  ! Pi
+   real (kind=8), parameter :: ZEPS  =  1.0e-16             ! Convergence criterion
+   integer                  :: jlat                         ! Latitudinal loop index
+   integer                  :: jiter                        ! Iteration loop index
+   real (kind=8)            :: z0,z1,z2,z3,z4,z5
+   !real (kind=8)            :: ql,qld ! XW(2017/4/11): use "contains" to incorporate ql and qld inside
+
+   ! Compute Gaussian abscissas & weights
+
+   z0 = PI / (2*klat+1)
+   z1 = 1.0_8 / (klat*klat*8)
+   z4 = 2.0_8 / (klat*klat)
+
+   do jlat = 1 , klat/2
+      z2 = z0 * (2*jlat - 0.5_8)
+      z2 = cos(z2 + z1 / tan(z2))
+      do jiter = 1 , NITER
+         z3 = ql(klat,z2) * qld(klat,z2)
+         z2 = z2 - z3
+         if (abs(z3) < ZEPS) exit ! converged
+      enddo ! jiter
+      z5 = ql(klat-1,z2) / sqrt(klat - 0.5_8)
+      pz0(jlat) = z2
+      pzw(jlat) = z4 * (1.0_8 - z2 * z2) / (z5 * z5)
+      pz0(klat-jlat+1) = -z2
+      pzw(klat-jlat+1) = pzw(jlat)
+   enddo ! jlat
+
+contains
+
+! ===========
+! FUNCTION QL
+! ===========
+
+   pure &
+   function ql(k,p)
+      implicit none
+      integer      , intent(IN) :: k
+      real (kind=8), intent(IN) :: p
+      real (kind=8)             :: ql
+
+      ! local
+      real (kind=8) :: z0,z1,z2,z3,z4
+      integer :: j
+
+      z0 = acos(p)
+      z1 = 1.0
+      z2 = 0.0
+
+      !XW(2017-4-8): avoid possibility of uninitialized use of z3 here,
+      !I add "if (k.ge.0)" structure to avoid uninitializaion of z3"
+      !if anything wrong with this, please remove the outter "if" structure.
+      if (k.ge.0) then
+         do j = k , 0 , -2
+            z3 = z1 * cos(z0 * j)
+            z2 = z2 + z3
+            z4 = (k-j+1) * (k+j) * 0.5_8
+            z1 = z1 * z4 / (z4 + (j-1))
+         enddo ! j
+         if (mod(k,2) == 0) z2 = z2 - 0.5_8 * z3
+      end if
+
+      z0 = sqrt(2.0_8)
+      do j = 1 ,k
+         z0 = z0 * sqrt(1.0_8 - 0.25_8 / (j*j))
+      enddo ! j
+      ql = z0 * z2
+   end function ql
+
+   ! ============
+   ! FUNCTION QLD
+   ! ============
+   pure &
+   function qld(k,p)
+      implicit none
+      integer      , intent(IN) :: k
+      real (kind=8), intent(IN) :: p
+      real (kind=8)             :: qld
+
+      ! local
+      real (kind=8) :: z
+      !real (kind=8) :: ql
+
+      z = p * ql(k,p) - sqrt((k + k + 1.0_8) / (k + k - 1.0_8)) * ql(k-1,p)
+      qld = (p * p - 1.0_8) / (k * z)
+   end function qld
+
+end subroutine inigau
